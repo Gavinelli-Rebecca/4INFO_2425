fd4bb94a47671a74a91bfb7c29ccb9a65b0b0434
Gavinelli-Rebecca <154589550+Gavinelli-Rebecca@users.noreply.github.com>
2025-05-09 13:29:33 +0200

    riaggiunti tutti gli esercizi

---
+++ TPSI/gavinelli_rebecca/SVELTE/WebApp/hello-word-svelte-kit~/src/routes/chi_sono/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import WhoAmI from '$lib/components/who_am_i.svelte' //'../../lib/components/who_am_i.svelte';
+</script>
+
+<WhoAmI nome="Rebecca" cognome="Gavinelli" eta="17" dataNascita="12/05/2007"/>
---
+++ TPSI/gavinelli_rebecca/SVELTE/WebApp/hello-word-svelte-kit~/src/routes/counter_page/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+    let contatore = 0;
+    let contatore_2 = 0;
+    import Counter from "$lib/components/counter.svelte";
+    import DoubleCounter from "$lib/components/double_counter.svelte";
+    let numeri = [1, 7, 3, 4, 5, 6, 8, 9, 10];
+    $:doppio = contatore * 2;
+
+    function gestisci_evento(evento) {
+        alert(`${evento.detail.tipo}-- ${evento.detail.valore}`);
+    }
+</script>
+
+<h1>
+    Pagina Counter
+</h1>
+
+{#if contatore < 5}
+    <h2 class="rosso">Il valore del contatore vale: {contatore}</h2>
+{:else}
+    <h2 class=sottolineato>Il valore del contatore vale: {contatore}</h2>
+{/if}
+
+<!-- 
+<h2>Il valore del contatore vale: {contatore}</h2> -->
+
+<Counter bind:counter={contatore} on:contatore={gestisci_evento}/>
+<!--  -->
+<!-- <h2>Il valore del contatore 2 vale: {contatore_2}</h2> -->
+<!--  -->
+<!-- <DoubleCounter bind:counter={contatore_2} /> -->
+
+
+<ul>
+  {#each numeri as numero, indice}
+    <li>numeri[{indice}]:{numero}</li>
+  {/each}
+</ul>
+
+
+<style>
+    .rosso {
+        color: red;
+    }
+
+
+    .sottolineato {
+        text-decoration : underline;
+    }
+</style>
